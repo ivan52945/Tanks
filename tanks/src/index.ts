e1930b3fe45f870cd801300f885626d2beadd67d
guschins <guschins@gmail.com>
2023-02-04 17:38:48 +0300

    feat: add StartSection

--- tanks/src/index.ts
+++ tanks/src/index.ts
@@ -1,37 +1,13 @@
 import Phaser from 'phaser';
-import logoImg from './assets/logo.png';
-
-class MyGame extends Phaser.Scene {
-    constructor() {
-        super('Game');
-    }
-
-    preload() {
-        this.load.image('logo', logoImg);
-    }
-
-    create() {
-        const logo = this.add.image(600, 150, 'logo');
-
-        this.tweens.add({
-            targets: logo,
-            y: 450,
-            duration: 2000,
-            ease: 'Power2',
-            yoyo: true,
-            loop: 1,
-        });
-    }
-}
-const windowInnerWidth = document.documentElement.clientWidth;
-const windowInnerHeight = document.documentElement.clientHeight;
+import './global.css';
+import StartScene from './ts/StartScene';
 
 const config = {
     type: Phaser.AUTO,
     parent: 'app',
-    width: windowInnerWidth - 20,
-    height: windowInnerHeight - 20,
-    scene: MyGame,
+    with: 600,
+    height: 800,
+    scene: StartScene,
 };
 
 const game = new Phaser.Game(config);
