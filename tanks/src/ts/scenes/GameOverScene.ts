fabbba686f5030cded120efe3c8489204084d07f
ivan-kaliaskin <jabr.don24@gmail.com>
2023-02-05 18:54:18 +0400

    feat: game over scene implemented

--- tanks/src/ts/scenes/GameOverScene.ts
+++ tanks/src/ts/scenes/GameOverScene.ts
@@ -1,6 +1,3 @@
-import logoImg from '../../assets/images/battle-city.png';
-import cursor from '../../assets/images/tank.png';
-
 class GameOverScene extends Phaser.Scene {
     keys: unknown;
 
@@ -11,25 +8,11 @@ class GameOverScene extends Phaser.Scene {
     }
 
     preload() {
-        this.add.text(100, 100, 'I- ', { font: '40px Pixel' });
-        this.add.text(250, 100, '00', { font: '40px Pixel' });
-        this.add.text(400, 100, 'HI- ', { font: '40px Pixel' });
-        this.add.text(550, 100, '00', { font: '40px Pixel' });
-        // this.load.image('logo', logoImg);
-        // this.load.image('cursor', cursor);
-        // this.add.text(350, 500, 'START GAME', { font: '32px Pixel' });
-        // this.add.text(350, 550, 'CONSTRUCTION', { font: '32px Pixel' });
-        // const red = this.add.text(400, 600, 'NAMCOT', { font: '40px Namco' });
-        // red.setTint(0x680000);
-        // this.add.text(150, 650, '© 1980 1985 NAMCO LTD.', { font: '32px Pixel' });
-        // this.add.text(180, 700, 'ALL RIGHTS RESERVED', { font: '32px Pixel' });
+        this.add.text(300, 300, 'GAME', { font: '90px Pixel', color: '#FF5000' });
+        this.add.text(300, 450, 'OVER', { font: '90px Pixel', color: '#FF5000' });
     }
 
     create() {
-        // this.add.image(500, 320, 'logo');
-        // this.input.keyboard.createCursorKeys();
-        // const player = this.add.image(300, 510, 'cursor');
-        // player.angle = 90;
         this.input.keyboard.on('keydown', (event: { key: string }) => {
             if (event.key === 'p') {   // ------- Инструмент разработчика. Переключатель сцен на англ. 'p'
                 this.scene.start('StartScene')
