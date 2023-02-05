00145e3f43b254e952562278ab6698ba39696793
guschins <guschins@gmail.com>
2023-02-05 11:45:50 +0300

    feat: add GameScene

---
+++ tanks/src/ts/GameScene.ts
@@ -0,0 +1,22 @@
+import logoImg from '../assets/images/battle-city.png';
+
+class GameScene extends Phaser.Scene {
+    keys: unknown;
+
+    constructor() {
+        super({
+            key: 'GameScene',
+        });
+    }
+
+    preload() {
+        this.load.image('logo', logoImg);
+    }
+
+    create() {
+        this.add.image(500, 320, 'logo');
+    }
+
+    update() {}
+}
+export default GameScene;
