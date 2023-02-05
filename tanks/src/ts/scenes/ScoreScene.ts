10ec1b841d8b1d3cb003929678e5ebda5f44072c
ivan-kaliaskin <jabr.don24@gmail.com>
2023-02-05 16:28:13 +0400

    feat: score scene implemented

--- tanks/src/ts/scenes/ScoreScene.ts
+++ tanks/src/ts/scenes/ScoreScene.ts
@@ -1,5 +1,9 @@
-import logoImg from '../../assets/images/battle-city.png';
-import cursor from '../../assets/images/tank.png';
+// import logoImg from '../../assets/images/battle-city.png';
+// import cursor from '../../assets/images/tank.png';
+import lightTank from '../../assets/images/gray-light-tank.png'
+import wheeledTank from '../../assets/images/gray-wheeled-tank.png'
+import mediumTank from '../../assets/images/gray-medium-tank.png'
+import heavyTank from '../../assets/images/gray-heavy-tank.png'
 
 class ScoreScene extends Phaser.Scene {
     keys: unknown;
@@ -11,25 +15,32 @@ class ScoreScene extends Phaser.Scene {
     }
 
     preload() {
-        this.add.text(100, 100, 'I- ', { font: '40px Pixel' });
-        this.add.text(250, 100, '00', { font: '40px Pixel' });
-        this.add.text(400, 100, 'HI- ', { font: '40px Pixel' });
-        this.add.text(550, 100, '00', { font: '40px Pixel' });
-        // this.load.image('logo', logoImg);
-        // this.load.image('cursor', cursor);
-        // this.add.text(350, 500, 'START GAME', { font: '32px Pixel' });
-        // this.add.text(350, 550, 'CONSTRUCTION', { font: '32px Pixel' });
-        // const red = this.add.text(400, 600, 'NAMCOT', { font: '40px Namco' });
-        // red.setTint(0x680000);
-        // this.add.text(150, 650, '© 1980 1985 NAMCO LTD.', { font: '32px Pixel' });
-        // this.add.text(180, 700, 'ALL RIGHTS RESERVED', { font: '32px Pixel' });
+        this.add.text(100, 60, 'HI-Score ', { font: '35px Pixel', color: '#FF4500' });
+        this.add.text(600, 60, '000000', { font: '35px Pixel', color: '#FFD700' }); // рекорд очков
+        this.add.text(300, 130, 'STAGE ', { font: '35px Pixel' });
+        this.add.text(520, 130, '00', { font: '35px Pixel' }); // номер пройденного уровня
+        this.add.text(10, 200, 'PLAYER', { font: '35px Pixel', color: '#FF4500' });
+        this.add.text(10, 270, '000000', { font: '35px Pixel', color: '#FFD700' }); // кол-во очков игрока за всю кампанию
+        this.add.text(10, 400, '0000 PTS', { font: '35px Pixel' }); // очки за лёгкий танк
+        this.load.image('lightTank', lightTank)
+        this.add.text(400, 400, '00-', { font: '35px Pixel' }); // сколько ЛТ уничтожено
+        this.add.text(10, 470, '0000 PTS', { font: '35px Pixel' }); // очки за колёсный танк
+        this.load.image('wheeledTank', wheeledTank)
+        this.add.text(400, 470, '00-', { font: '35px Pixel' }); // сколько КТ уничтожено
+        this.add.text(10, 540, '0000 PTS', { font: '35px Pixel' }); // очки за средний танк
+        this.load.image('mediumTank', mediumTank)
+        this.add.text(400, 540, '00-', { font: '35px Pixel' }); // сколько СТ уничтожено
+        this.add.text(10, 610, '0000 PTS', { font: '35px Pixel' }); // очки за тяжёлый танк
+        this.load.image('heavyTank', heavyTank)
+        this.add.text(400, 610, '00-', { font: '35px Pixel' }); // сколько ТТ уничтожено
+        this.add.text(190, 680, 'TOTAL 00', { font: '35px Pixel' }); // всего уничтожено
     }
 
     create() {
-        // this.add.image(500, 320, 'logo');
-        // this.input.keyboard.createCursorKeys();
-        // const player = this.add.image(300, 510, 'cursor');
-        // player.angle = 90;
+        this.add.image(540, 410, 'lightTank');
+        this.add.image(540, 480, 'wheeledTank');
+        this.add.image(540, 550, 'mediumTank');
+        this.add.image(540, 625, 'heavyTank');
         this.input.keyboard.on('keydown', (event: { key: string }) => {
             if (event.key === 'p') {   // ------- Инструмент разработчика. Переключатель сцен на англ. 'p'
                 this.scene.start('HiscoreScene')
