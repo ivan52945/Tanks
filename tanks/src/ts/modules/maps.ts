e63401c9f24032eaa0a90cfa7792c4c376ccd992
ivan52945 <ivan52945@gmail.com>
2023-02-18 14:52:58 +0300

    fix: bugs and add changing map

---
+++ tanks/src/ts/modules/maps.ts
@@ -0,0 +1,7 @@
+import level1 from '../../assets/maps/tilemap1.json';
+import level2 from '../../assets/maps/tilemap2.json';
+import level3 from '../../assets/maps/tilemap3.json';
+
+const maps = [level1, level2, level3];
+
+export default maps;
