1e514f541b688c68a4806125acc6f01e9a6b21f4
guschins <guschins@gmail.com>
2023-02-26 15:51:21 +0300

    feat: add hi score logic

--- tanks/src/ts/scenes/HiscoreScene.ts
+++ tanks/src/ts/scenes/HiscoreScene.ts
@@ -1,5 +1,7 @@
 class HiscoreScene extends Phaser.Scene {
     keys: unknown;
+    private stageNumber = 1;
+    private hiScore = 0;
 
     constructor() {
         super({
@@ -7,12 +9,21 @@ class HiscoreScene extends Phaser.Scene {
         });
     }
 
+    init(result: { stage: number; hiScore: number }) {
+        this.stageNumber = result.stage || this.stageNumber;
+        this.hiScore = result.hiScore;
+    }
+
     preload() {
         this.add.text(200, 200, 'HISCORE ', { font: '90px Pixel' });
-        this.add.text(300, 350, '000000', { font: '90px Pixel' });
+        this.add.text(300, 350, `${this.hiScore}`, { font: '90px Pixel' });
     }
 
     create() {
+        setTimeout(() => {
+            this.scene.start('StageNumberScene', { stage: this.stageNumber });
+        }, 3000);
+
         this.input.keyboard.on('keydown', (event: { key: string }) => {
             if (event.key === 'q') {
                 // ------- Инструмент разработчика. Переключатель сцен на англ. 'q'
